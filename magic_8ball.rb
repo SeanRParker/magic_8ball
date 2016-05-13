f88305ff49c87e7642be7b24d37bc7ded2d055a7
Shaws <seanrichardparker@gmail.com>
2016-05-13 14:24:35 -0600

    Enable easter egg menu and array manipulation
    Next step: Duplicate and array to store new values to and enable a reset option to the old menu.

--- magic_8ball.rb
+++ magic_8ball.rb
@@ -1,19 +1,3 @@
-# Prompt user to ask a question or type quit to exit.
-puts "Welcome to Magic 8 Ball"
-
-def welcome_message
-  puts """
-  What would you like to know? \n
-  Enter a yes or no question...
-  or type 'quit' to leave. \n
-  """
-  @question = gets.chomp
-  puts "Fine. I didn't want to talk anyway!" if @question == "quit" || @question == "QUIT"
-  exit(0) if @question == "quit" || @question == "QUIT"
-end
-
-welcome_message
-
 # Array of stored responses
 @responses = [
   "You would ask that wouldn't you?",
@@ -44,6 +28,54 @@ welcome_message
   "You win. Sure."
 ]
 
+# Add a question
+def easter_eggs
+  while true
+    puts "Welcome to the secret menu!!"
+    puts "1) Add a new response"
+    puts "2) See available responses"
+    puts "3) Return to main menu"
+    easter_input = gets.chomp
+    case
+    when easter_input == "1"
+      new_answer = gets.strip
+      @responses << "#{new_answer}"
+      easter_eggs
+    when easter_input == "2"
+      puts @responses.join(" \n")
+      easter_eggs
+    when easter_input == "3"
+      welcome_message
+      break if easter_input == "3"
+    end
+  end
+
+
+end
+
+# Prompt user to ask a question or type quit to exit.
+puts "Welcome to Magic 8 Ball"
+
+def welcome_message
+  puts """
+  What would you like to know? \n
+  Enter a yes or no question...
+  or type 'quit' to leave. \n
+  """
+  @question = gets.chomp
+  puts "Fine. I didn't want to talk anyway!" if @question == "quit" || @question == "QUIT"
+  exit(0) if @question == "quit" || @question == "QUIT"
+  easter_eggs if @question == "is this the real life?"
+  # Reset responses array if user types reset_answers
+  # print responses if user types print_answers using .join
+  puts @responses.join("-") if @question == "print_answers"
+end
+
+
+welcome_message
+
+
+
 def output
   # Randomizes response and stores it in a value
   answer = @responses.sample
@@ -63,6 +95,7 @@ def ask_again
   while true
     puts "Would you like to ask another question? (y/n)"
     again = gets.strip.downcase
+    puts "Thanks for using Magic 8 ball." if again == "n" || again == "no" || again == "NO" || again == "No"
     exit (0) if again == "n" || again == "no" || again == "NO" || again == "No"
     welcome_message
     output
